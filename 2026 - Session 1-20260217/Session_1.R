d3794fe40fdf91f273d10af2c2572a7ff4a03073
Leonel Fernando Nabaza Ruibal <leonelfernando.nabaza@estudiants.urv.cat>
2026-02-17 16:43:22 +0100

    initial commit

---
+++ 2026 - Session 1-20260217/Session_1.R
@@ -0,0 +1,76 @@
+
+## ------------------------------------------------------------------------
+library(ggplot2)
+library(dplyr)
+
+theme_set(theme_bw())
+
+# 1. Statistical model
+
+# Plot binomial distribution
+
+
+
+# Exercise 1.5
+
+
+
+
+# 3. Bayesian model
+
+# Exercise coin
+
+
+
+# Exercise world
+
+
+
+## ------------------------------------------------------------------------
+
+# 4. Likelihood function
+
+# Exercise likelihood:
+
+
+
+
+
+## ------------------------------------------------------------------------
+## Predictive simulation
+
+# Simulate 10000 draws from prior density: p_sim
+
+# Simulate 10000 draws from the predictive density: y_sim
+
+# Plot the prior predictive density
+
+# Compute the probability of less than 5 tails
+
+# Find a 90 percent prediction interval for the number of tails
+
+
+
+
+# Exercise 1.4
+
+# a) Choose the parameters of a conjugate prior distribution (gamma), 
+# and explain why you choose them (it might be useful to draw the 
+# prior predictive distribution to back your choice up).
+
+
+
+# b) Draw in the same graph the prior distribution and the 
+# likelihood function.
+
+
+
+# c) Draw the prior predictive distribution.
+
+
+
+# Now, assume that the members of the association know nothing 
+# about the number of weekly visitors:
+# d) Choose the parameters of a conjugate prior distribution in 
+# that case.
+
